b71014a6a11c4f7b7882180591657e479e37c4a4
Shiv Prasad <shiv@param.ai>
2018-04-15 02:27:44 +0530

    Added Query builder method for link extractions

--- scrapper/query_builder.py
+++ scrapper/query_builder.py
@@ -1,12 +1,20 @@
 import config as settings
 
 
-def fetch_job_link_url(query, location, page_count):
-    return "{base_uri}/jobs?q={query}&l={location}&start={page_count}".format(base_uri=settings.JOB_BASE_URL, query=query, location=location, page_count=page_count)
+def fetch_job_link_url(queries, page_count):
+    url = "{base_uri}?".format(base_uri=settings.JOB_BASE_URL)
+    for query in queries.keys():
+        url += "{key}={value}&".format(key=query, value=queries[query])
+    url += "start={page_count}".format(page_count=page_count)
+    return url
 
 
-def fetch_resume_link(query, location, page_count):
-    return "{base_uri}/resumes?q={query}&l={location}&start={page_count}".format(base_uri=settings.RESUME_BASE_URL, query=query, location=location, page_count=page_count)
+def fetch_resume_link(queries, page_count):
+    url = "{base_uri}?".format(base_uri=settings.RESUME_BASE_URL)
+    for query in queries.keys():
+        url += "{key}={value}&".format(key=query, value=queries[query])
+    url += "start={page_count}".format(page_count=page_count)
+    return url
 
 
 def fetch_job_url(link):
