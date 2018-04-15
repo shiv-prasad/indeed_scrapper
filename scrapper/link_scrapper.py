9507cfdeb454762606c23215efad4d6b120f68bf
Shiv Prasad <shiv@param.ai>
2018-04-15 23:27:28 +0530

    made pagination a module

--- scrapper/link_scrapper.py
+++ scrapper/link_scrapper.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 import db
+import pagination
 
 headers = {'User-Agent': UserAgent().chrome}
 
@@ -12,34 +13,6 @@ def get_soup(url):
 
     page = requests.get(url, headers=headers)
     return BeautifulSoup(page.text, 'html.parser')
-
-
-def make_start_list(for_task, total_results):
-
-    starts = []
-
-    if for_task == settings.TASKS[1]:
-        max_results = settings.MAX_RESULTS_FOR_RESUME
-        max_page_results = settings.MAX_RESULTS_PER_RESUME_PAGE
-
-    else:
-        max_results = settings.MAX_RESULTS_FOR_JOB
-        max_page_results = settings.MAX_RESULTS_PER_JOB_PAGE
-
-    if total_results > max_results:
-        total_results = max_results - 1
-        print "# {max}+ results found. Fetching first {total} results".format(max=max_results, total=total_results + 1)
-
-    total_pages = (total_results / max_page_results)
-    if total_results % max_page_results > 0:
-        total_pages += 1
-
-    print "# Total Pages: <{total}>".format(total=total_pages)
-
-    for i in range(0, total_pages):
-        starts.append(max_page_results * i)
-
-    return starts
 
 
 def get_links_from_page(for_task, url):
@@ -131,7 +104,7 @@ def fetch_links(for_task, queries):
                 print "# Result Present: <True>"
                 print "# Total Results: <{total}>".format(total=total_results)
 
-                starts = make_start_list(for_task, total_results)
+                starts = pagination.make_start_list(for_task, total_results)
 
             else:
 
